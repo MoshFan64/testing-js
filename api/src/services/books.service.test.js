02a0cc697fb26c5f5dd1083d36b8946d6d15a84e
MoshFan64 <canhel.balam@gmail.com>
2025-06-11 19:35:53 -0600

    Code added for createBooks, mock test commentated.

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -2,20 +2,23 @@
 const BooksService = require('./books.service')
 
 /*Suplantando la clase MongoLib*/
-const MongoLibStub = {
-  getAll: () => [...fakeBooks]
-}
+//const MongoLibStub = {
+  //getAll: () => [...fakeBooks],
+  //create: jest.fn((collection, data) => {
+    //return Promise.resolve({ _id: '123456', ...data });
+  //})
+//}
 
 /*Datos simulados*/
-const fakeBooks = [
-  {
-    _id: 1,
-    name: 'Harry Potter'
-  }
-]
+//const fakeBooks = [
+  //{
+    //_id: 1,
+    //name: 'Harry Potter'
+  //}
+//]
 
 /*Llamada a mock*/
-jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+//jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
 
 describe('Test for BooksService.', () => {
   /*Variable para la instancia del servicio*/
@@ -26,23 +29,34 @@ describe('Test for BooksService.', () => {
     service = new BooksService();
   });
 
-/*Pruebas para el método getBook*/
-
+  /*Pruebas para el método getBooks*/
   describe('Test for getBooks.', () => {
-  test('Should return a list of books.', async () => {
-    /*Arrange: preparar el entorno*/
-    /*Act: acción a probar*/
-    const books = await service.getBooks({});
-    console.log(books);
-    /*Assert: confirmar*/
-    expect(books.length).toEqual(1);
+    test('Should return a list of books.', async () => {
+      /*Arrange: preparar el entorno*/
+      /*Act: acción a probar*/
+      const books = await service.getBooks({});
+      console.log(books);
+      /*Assert: confirmar*/
+      expect(books.length).toEqual(2);
+    });
   });
-});
-/*describe('Test for createBooks.', () => {
-  test('Should create a new book.', async () => {
 
+  /*Pruebas para el método createBooks*/
+  describe('Test for createBooks.', () => {
+    test('Should create a new book.', async () => {
+      const newBook = {
+        title: '1984',
+        author: 'George Orwell',
+        year: 1949
+      };
+
+      const createdBook = await service.createBook(newBook);
+      console.log(createdBook);
+
+      expect(createdBook).toBeDefined();
+      expect(createdBook._id).toBeDefined();
+      expect(createdBook.title).toBe(newBook.title);
+    });
   });
-})
-*/
 
 });
